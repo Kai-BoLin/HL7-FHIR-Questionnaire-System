b89fbd82f297d87d813f47a928143e8f82273498
KaiBo <kaibo19960528@gmail.com>
2026-07-26 12:32:49 +0800

    1.在server中npm install axios  2.症狀欄中新增流鼻水跟腹瀉症狀  3.透過Postman上傳每日健康問卷調查 (問卷ID : 137202531)  4.新增一個「通用代理路由」，無論前端想查 Patient、Observation 還是 Condition，都透過這個窗口。  5.使用Proxy  5.1宣告axios並啟用 const axios = require('axios');  　5.1.1全域變數設定 const FHIR_BASE_URL = "https://hapi.fhir.org/baseR4"  　5.1.2宣告const fhirProxyHandler，並註解掉原本問卷改從fhir.org網站取得  6.建立新的get      app.get("/api/fhir/:resource" , fhirProxyHandler);      app.get("/api/fhir/:resource/id" , fhirProxyHandler);  7.註解掉onMounted(loadQuestionnaire);    7.1 新增const targetId = ref ('') 空值要輸入Postman的問卷ID    7.2 新增fatchQuestionnaire函式

---
+++ Questionnaire-threeth/fhir-project/client/src/App.vue
@@ -0,0 +1,75 @@
+<script setup>
+import {ref , onMounted} from 'vue';                    //onMounted開啟瀏覽器就會去執行此動作
+import BaseInput from './components/BaseInput.vue';     //引入template的內容
+
+const questionnaire = ref (null); 
+const answers = ref ({});
+const targetId = ref('');
+const loadQuestionnaire = async () => {                               //拿取資料的動作
+const res = await fetch ('http://localhost:3000/questionnaire');      //從這個api拿取資料
+const data = await res.json();                                        //json後存取到data中
+  questionnaire.value=data;
+  data.item.forEach(q=>{                                              //forEach跑完所有內容才停下來
+    answers.value[q.linkId]=null;                                     //用linkId串起來，建立每個問題的答案
+  });
+};
+
+const fetchQuestionnaire = async () => {
+  if(!targetId.value) return;
+  questionnaire.value=null;
+  try{
+    const res = await fetch (
+      `http://localhost:3000/api/fhir/Questionnaire/${targetId.value}`
+    );
+    if(!res.ok)
+    throw new Error("找不到此ID問卷");
+    const data = await res.json();
+    questionnaire.value = data;
+    answers.value = {};
+    if(data.item){
+      data.item.forEach(q => {
+        answers.value[q.linkId]=null;
+      });
+    }
+  }catch(error){
+    console.error("取得問卷發生錯誤");
+  }
+}
+
+//onMounted(loadQuestionnaire);                          //開啟瀏覽器就執行此問卷
+</script>
+
+<template>
+
+  <div>
+    <input v-model="targetId" placeholder="請輸入問卷ID">
+    <button @click="fetchQuestionnaire"> 載入問卷 </button>
+  </div>
+
+  <div v-if="questionnaire">
+    <h1> {{ questionnaire.title }} </h1>
+    <form @submit.prevent="onSubmitForm">                          <!--@submit.prevent不讓網頁刷新介面-->
+      <BaseInput
+        v-for = "item in questionnaire.item"
+        :key = "item.linkId"
+        :label = "item.text"
+        :type = "item.type === 'decimal'?'number' : item.type"      
+        :options = "item.answerOption"
+        v-model = "answers[item.linkId]">                           <!--傳送到子組件 modelValue = answer[item.linkId]-->
+        <!--
+        v-for，當item出現幾次就會回圈幾次直到所有跑完。
+        key代表會連接到題目出現的linkId
+        label代表item的text會出現題目的文字
+        type出現decimal(因為HTML沒有decimal，FHIR才有的內容)時，轉換成number
+        answerOption會出現在option中
+        item.linked會存在answer中
+        -->
+      </BaseInput>
+      <button type="submit"> 送出 </button>
+    </form>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
